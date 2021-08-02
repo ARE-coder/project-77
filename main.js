a097327f31b3fd8b7546793a95b527613cce8e33
ARE-coder <82109141+ARE-coder@users.noreply.github.com>
2021-08-02 12:52:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+var first = "Areeb"; 
+var last = "Shah"; 
+var name = first.concat(last); 
+console.log( name );
+
+var a = 5; 
+var b = 10; 
+var add = a + b;
+console.log(add);
+var multiply = a * b;
+console.log(multiply);
+var divide = b / a; 
+console.log( divide );
+var subtract = b - a; 
+console.log( subtract );     
+
+var full_length_of_name = name.length;
+console.log(full_length_of_name)
+
+var new_name = "I'am Areeb Shah";
+var replace = new_name.replace("Areeb", "Mohamed" );
+console.log(replace);
+var rocky = "ROCKY";
+var lowercase = rocky.toLowerCase();
+console.log(lowercase)
+var Areeb = "areeb";
+var uppercase = Areeb.toUpperCase();
+console.log(uppercase)
+
+var sqrt = Math.sqrt(64)
+console.log(sqrt)
+
+var min = Math.min(20, 0)
+console.log(min)
+var max = Math.max(20,0)
+console.log(max)
+
+var pow = Math.pow(4, 2)
+console.log(pow)
